2111d19cdf40917b5226398aa51f479c6a0cc367
Samuel-Harwood <101341494+Samuel-Harwood@users.noreply.github.com>
2022-03-27 13:29:09 +0100

    Code Review - Samuel Harwood

---
+++ CMP1903M Sam Review/CMP1903M Assessment 1 Base Code/Input.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903M_Assessment_1_Base_Code
+{
+    public class Input
+    {
+        //Handles the text input for Assessment 1
+         string text = "nothing";
+        
+        //Method: manualTextInput
+        //Arguments: none
+        //Returns: string
+        //Gets text input from the keyboard
+        public string manualTextInput()
+        {
+            Console.Write("Please Input Text at the Prompt> ");//User Prompt
+            string text = Console.ReadLine()?? string.Empty;//Reads User Input up to 1st Newline Character.
+            return text;
+        }
+
+        //Method: fileTextInput
+        //Arguments: string (the file path)
+        //Returns: string
+        //Gets text input from a .txt file
+        public string fileTextInput(string fileName)
+        {
+            {
+         
+                try
+                {
+                    var text = File.ReadAllText(fileName); //Reads Text of File from Path provided.
+                    Console.WriteLine(text); //Outputs the Text for to confirm the text is what is wanted by the user.
+                }
+                catch (IOException)//Handles when the File Path is un-proccessable.
+                {
+                    Console.WriteLine("File Open NOT Working.\nSwitching to Manual Mode.");
+                    manualTextInput();
+                }
+                
+
+                
+            }
+            return text;
+        }
+
+    }
+}
