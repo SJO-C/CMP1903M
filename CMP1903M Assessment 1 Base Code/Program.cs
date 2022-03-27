c9634c811e97412cc9c010c38570e5b78ad135c8
Samuel Orman-Chan <samorman.chan@outlook.com>
2022-03-27 21:59:51 +0100

    Another Minor Change on File Picker Greeting.
    Signed-off-by: Samuel Orman-Chan <samorman.chan@outlook.com>

--- CMP1903M Assessment 1 Base Code/Program.cs
+++ CMP1903M Assessment 1 Base Code/Program.cs
@@ -38,10 +38,11 @@ namespace CMP1903M_Assessment_1_Base_Code
                 try //Tries to open a file path.
                 {
                     Console.WriteLine("\nFile Mode Selected.");
-                    Console.WriteLine("\nIf your text file is not in the same directory as this Executable (available below).\nThen please enter the full path.\n");
+                    Console.WriteLine("\nIf your text file is not in the same directory as this Executable (available below).\nThen please enter the full path.");
                     Console.WriteLine("\nYou are currently in Directory: " + Directory.GetCurrentDirectory()); //Outputs current Directory to allow for the user to know where the program is executing from.
-                    Console.Write("Enter File Path > ");
-                    string path2File = Console.ReadLine();//File Path Getter.
+                    Console.Write("\nEnter File Path > ");
+                    string path2File;
+                    path2File = Console.ReadLine();//File Path Getter.
                     path2File = path2File.Replace(@"\", "/");
                     path2File = path2File.Replace("\"", "");
                     textDataforAnalysis = TInput.fileTextInput(path2File);//Passes File Path String to Input Program.
